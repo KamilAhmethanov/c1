0a81abc915bb263391531ba773ab4bc3ed8e22ce
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-05 18:02:15 +0300

    dz2

---
+++ dz№2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите еще одно целое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) 
+{
+    Console.WriteLine($"Max = {a}");
+}
+else
+{
+    Console.WriteLine($"Max = {b}");
+}
+
+
