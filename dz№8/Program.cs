fcb136b31914f3295cec42c25f901bc13db2399b
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-05 18:52:07 +0300

    dz8

---
+++ dz№8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+while (a > b)
+{
+    b = b + 2;
+    Console.Write($"{b} ");
+}
+if (a < 1)
+{
+    Console.WriteLine("Ошибка");
+}
