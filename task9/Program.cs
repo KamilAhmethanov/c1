6553eeb3aa69f71284a4935059fec860998ee258
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-08 20:56:25 +0300

    task9 done.

--- task9/Program.cs
+++ task9/Program.cs
@@ -3,8 +3,9 @@ int a1 = a / 10;
 int a2 = a % 10;
 int Max = a1;
 Console.WriteLine($"Случайное число в диапозоне от 10 до 99: {a}");
-if (a2>=a1) Console.WriteLine($"Наибольшая цифра числа: {a2}");
+if (a2==a1) Console.WriteLine("Цифры равны");
+else if (a2>a1) Console.WriteLine($"Наибольшая цифра числа: {a2}");
 else
 {
-    Console.WriteLine($"Наибольшая цифра числа: {a1}");
+    Console.WriteLine($"Наибольшая цифра числа {a} : {a1}");
 }
