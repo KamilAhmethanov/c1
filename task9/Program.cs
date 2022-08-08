c3fb73c548a36a1f7ff5d0ee0101518c614870bb
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-08 20:20:47 +0300

    task9 done

---
+++ task9/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = new Random().Next(10, 99);
+int a1 = a / 10;
+int a2 = a % 10;
+int Max = a1;
+Console.WriteLine(a);
+if (a2>=a1) Console.WriteLine(a2);
+else
+{
+    Console.WriteLine(a1);
+}
