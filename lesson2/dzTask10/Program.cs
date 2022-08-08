dc68515960721ab40e72dad5f175951e61cc6881
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-08 21:46:01 +0300

    task10 done.

---
+++ lesson2/dzTask10/Program.cs
@@ -0,0 +1,4 @@
+﻿int number = new Random().Next(100, 999);
+int number2 = number / 10 % 10;
+Console.WriteLine($"Случайное число в диапозоне от 100 до 999: {number}");
+Console.WriteLine($"Вторая цифра случайного числа {number} -> {number2}");
