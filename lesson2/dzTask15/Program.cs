1f80d29981fe0449950111b0b1594ffc98d1b8a1
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-11 14:18:48 +0300

    done task15

---
+++ lesson2/dzTask15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число от 1 до 7 :");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day >= 8 || day <= 0) Console.WriteLine("Ошибка");
+else if (day > 0 && day < 6) Console.WriteLine("нет");
+else if (day > 5 && day < 8) Console.WriteLine("да");
