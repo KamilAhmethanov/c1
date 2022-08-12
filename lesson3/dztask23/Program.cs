12e75afc6c2adc3324d8a3552e522a682d5ebb3b
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-12 21:40:51 +0300

    task23 done

---
+++ lesson3/dztask23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (num > 1)
+{
+    while (i <= num)
+    {
+        int z = i * i * i;
+        Console.WriteLine($"{i}  {z}");
+        i++;
+    }
+}
+else Console.WriteLine("Число меньше 1");
