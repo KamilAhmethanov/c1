42f4ad9aa95eed1b0f87fe06b80a3530375e8224
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-20 19:41:17 +0300

    task25 done

---
+++ lesson4/dzTask25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int c = a;
+while (b > i)
+{
+    c = c * a;
+    i++;
+}
+Console.WriteLine(c);
