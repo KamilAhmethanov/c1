783ee00ac87cb89599da5e97b5ea4cd8cd632eac
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-20 20:18:33 +0300

    task29 done

---
+++ lesson4/dzTask29/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] NewArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < 7; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+int[] Array = NewArray();
+void PrintArray()
+{
+    for (int i = 0; i < 7; i++)
+    {
+        Console.Write($" {Array[i]}, ");
+    }
+}
+PrintArray();
