ee6dd8b98c7da66c205c0b0e91e0886e03a41405
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-20 20:00:40 +0300

    task27 done

---
+++ lesson4/dzTask27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int sm1 = 0;
+while (num > 0)
+{
+    sm1 = num % 10;
+    num /= 10;
+    sum = sum + sm1;
+}
+Console.WriteLine(sum);
