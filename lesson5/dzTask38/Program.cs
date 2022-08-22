a07477a05f951d63b06455377ab1556eb64a9b1e
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-22 21:36:05 +0300

    Task38 done

---
+++ lesson5/dzTask38/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+FillArrayRandomNumbers(numbers);
+void FillArrayRandomNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(new Random().Next(100, 999)) / 100;
+    }
+}
+void PrintArray(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+}
+Console.Write("Массив: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+    {
+        max = numbers[i];
+    }
+    if (numbers[i] < min)
+    {
+        min = numbers[i];
+    }
+}
+double def = max - min;
+Console.Write($"-> {def}");
