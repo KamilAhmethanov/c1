b9a095217ec0f0d96bf37c01ae39e326170e90ec
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-22 21:06:06 +0300

    Task34 done

---
+++ lesson5/dzTask34/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+PrintArray(numbers);
+int count = 0;
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 999);
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+    if (numbers[i] % 2 == 0)
+        count++;
+
+void PrintArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+}
+Console.WriteLine($"-> {count}");
