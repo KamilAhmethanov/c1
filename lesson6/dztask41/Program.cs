37c2de414fa1e8fc26e382035ac8628c5c18140b
KamilAhmethanov <camilaxnk@gmail.com>
2022-08-28 13:11:39 +0300

    Task41 done

---
+++ lesson6/dztask41/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите числа c пробелами: ");
+int[] intArray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < intArray.Length; i++)
+{
+    if (intArray[i] > 0) count++;
+}
+Console.WriteLine($"Чисел большe нуля -> {count}");
