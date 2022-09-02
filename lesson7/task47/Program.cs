d3eee8b9ab49735b314aac3f1b0247e80794bffa
KamilAhmethanov <camilaxnk@gmail.com>
2022-09-02 21:07:02 +0300

    Task47 done

---
+++ lesson7/task47/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] Array = new double[m, n];
+void CreateArray(double[,] Array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+CreateArray(Array);
+void WriteArray(double[,] Array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double nmbs = Math.Round(Array[i, j], 1);
+            Console.Write($"{nmbs} ");
+        }
+        Console.WriteLine();
+    }
+}
+WriteArray(Array);
